5dec3ace287b36f49649ee0f16e95b5f8138cf1f
dvcsr <4th3sh0ws@gmail.com>
2024-09-07 12:40:39 +0700

    finished remove all odd

--- src/RemovaAllOdd.java
+++ src/RemovaAllOdd.java
@@ -1,5 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class RemovaAllOdd {
     public static void main(String[] args) {
-        System.out.println("test push");
+        List<Integer> numList = Stream.of(1,2,3,4,5,6,7,8,9,10).collect(Collectors.toList());
+        System.out.println(numList);
+
+        List<Integer> oddRemoved = numList.stream().filter(e -> (e % 2) == 0).collect(Collectors.toList());
+        System.out.println(oddRemoved);
     }
 }
